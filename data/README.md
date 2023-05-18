6606ee30ca5c22c00727b1e7b0893c1a73ccd588
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-05-18 12:03:17 +0100

    document data

---
+++ data/README.md
@@ -0,0 +1,33 @@
+
+# Data for vaccine-product-based (VPB) and platform-based (PFB) analysis
+
+## Outcomes:
+
+* documented COVID-19 infection (`docinfect`)
+  - `t[,i]`: time point (weeks?)
+  - `r[,i]`: number of infections?
+  - `n[,i]`: sample size?
+  - `na[]`: Number of time points
+* symptomatic COVID-19 infection (`symp`)
+* severe COVID-19 infection (`severe`)
+* COVID-19 related hospitalization (`hosp`)
+* COVID-19 related death (`death`)
+
+## Parameters:
+
+* `d`:log odds of intervention k
+* `lor`:log odds ratio
+* `rk`:ranking
+* `sdALL`:between-design heterogeneity
+* `sdRCT`:heterogeneity among RCTs
+* `sdRC`:heterogeneity among retrospective cohort studies
+* `sdPC`:heterogeneity among prospective cohort studies
+* `sdCC`:heterogeneity among case-control studies
+
+## Abbreviations:
+
+* Randomized controlled trial (RCT)
+* Case-control study (CC)
+* Retrospective cohort study (RC)
+* Prospective cohort study (PC)
+* Vaccine effectiveness (VE)
--- scripts/R2jags.R
+++ scripts/R2jags.R
@@ -1,24 +1,50 @@
+
+# original script
+# from Au (2022)
+
 library(R2jags)
 library(dplyr)
 
-setwd("\PATH\TO\FILE")
 
-read.datafile<-("export_VPB_main_docinfect.csv")
-datafile<-read.csv(read.datafile, header = TRUE, fileEncoding = "UTF-8-BOM")
-nvac<-max(datafile[,ncol(datafile)])
-data<-list(ntALL=max(cbind(datafile[,1:nvac]), na.rm = TRUE), t=cbind(datafile[,1:nvac]),r=cbind(datafile[,(nvac+1):(nvac*2)]),
-               n=cbind(datafile[,(nvac*2+1):(nvac*3)]),na=datafile[,ncol(datafile)])
-para<-c("d", "sdALL", "sdRCT", "sdCC", "sdRC", "sdPC", "lor", "rk")
-file<-("VPB_main_docinfect.bugs")
+filename <- ("data/export_VPB_main_docinfect.csv")
+dat_raw <-
+  read.csv(filename, header = TRUE, fileEncoding = "UTF-8-BOM")
 
+nvac <- max(dat_raw[, ncol(dat_raw)])
 
-jagsfit<-jags(data = data, inits = NULL, parameters.to.save = para, 
-     model.file= file, n.chains=3, n.iter=100000, n.burnin=floor(100000/5),
-     n.thin=max(1, floor((100000 - floor(100000/5)) / 1000)),
-     DIC=TRUE, working.directory=NULL, jags.seed = 123,
-     refresh = 100000/50, progress.bar = "text", digits=5,
-     RNGname = c("Wichmann-Hill", "Marsaglia-Multicarry",
-                 "Super-Duper", "Mersenne-Twister"),
-     jags.module = c("glm","dic"), quiet = FALSE)
+data <-
+  list(
+    ntALL = max(cbind(dat_raw[, 1:nvac]), na.rm = TRUE),
+    t = cbind(dat_raw[, 1:nvac]),
+    r = cbind(dat_raw[, (nvac + 1):(nvac * 2)]),
+    n = cbind(dat_raw[, (nvac * 2 + 1):(nvac * 3)]),
+    na = dat_raw[, ncol(dat_raw)])
+
+para <- c("d", "sdALL", "sdRCT", "sdCC", "sdRC", "sdPC", "lor", "rk")
+bugs_filename <- "VPB_main_docinfect.bugs"
+
+jagsfit <- jags(
+  data = data,
+  inits = NULL,
+  parameters.to.save = para,
+  model.file = bugs_filename,
+  n.chains = 3,
+  n.iter = 100000,
+  n.burnin = floor(100000 / 5),
+  n.thin = max(1, floor((
+    100000 - floor(100000 / 5)) / 1000)),
+  DIC = TRUE,
+  working.directory = NULL,
+  jags.seed = 123,
+  refresh = 100000 / 50,
+  progress.bar = "text",
+  digits = 5,
+  RNGname = c(
+    "Wichmann-Hill",
+    "Marsaglia-Multicarry",
+    "Super-Duper",
+    "Mersenne-Twister"),
+  jags.module = c("glm", "dic"),
+  quiet = FALSE)
 
 traceplot(jagsfit)
