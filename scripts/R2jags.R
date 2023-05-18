a6bbe1c1255d46e271a0d43639d13255a0412eec
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-05-18 18:53:09 +0100

    started to clean code

--- scripts/R2jags.R
+++ scripts/R2jags.R
@@ -10,18 +10,19 @@ filename <- ("data/export_VPB_main_docinfect.csv")
 dat_raw <-
   read.csv(filename, header = TRUE, fileEncoding = "UTF-8-BOM")
 
-nvac <- max(dat_raw[, ncol(dat_raw)])
+# maximum number time points across studies
+max_t_idx <- max(dat_raw$na..)
 
 data <-
   list(
-    ntALL = max(cbind(dat_raw[, 1:nvac]), na.rm = TRUE),
-    t = cbind(dat_raw[, 1:nvac]),
-    r = cbind(dat_raw[, (nvac + 1):(nvac * 2)]),
-    n = cbind(dat_raw[, (nvac * 2 + 1):(nvac * 3)]),
-    na = dat_raw[, ncol(dat_raw)])
+    ntALL = max(dat_raw[, 1:max_t_idx], na.rm = TRUE),  # maximum time across all studies
+    t = dat_raw[, 1:max_t_idx],
+    r = dat_raw[, (max_t_idx + 1):(max_t_idx * 2)],
+    n = dat_raw[, (max_t_idx * 2 + 1):(max_t_idx * 3)],
+    na = dat_raw$na..)
 
 para <- c("d", "sdALL", "sdRCT", "sdCC", "sdRC", "sdPC", "lor", "rk")
-bugs_filename <- "VPB_main_docinfect.bugs"
+bugs_filename <- "BUGS/VPB_main_docinfect.bugs"
 
 jagsfit <- jags(
   data = data,
