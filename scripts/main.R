81fcb67819f392748455540c33759d9c642a2dea
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-05-21 17:19:56 +0100

    delete old files

--- scripts/main.R
+++ scripts/main.R
@@ -1,29 +1,58 @@
 
-# original script
+# modified script
 # from Au (2022)
-
 
 library(R2jags)
 library(dplyr)
 
+# 1: RCT
+# 2: retrospective cohort studies
+# 3: prospective cohort studies
+# 4: case-control
 
-filename <- ("data/export_VPB_main_docinfect.csv")
+# select data
+filename <- ("data/export_VPB_main_docinfect_v2.csv")
+
 dat_raw <-
   read.csv(filename, header = TRUE, fileEncoding = "UTF-8-BOM")
 
-# maximum number time points across studies
-max_t_idx <- max(dat_raw$na..)
+# times across studies
+times <- dat_raw[, grepl("t\\d", names(dat_raw))]
+na <- apply(times, 1, \(x) sum(!is.na(x)))  # number of points 
+max_t_idx <- max(na)
+
+tlong <- 
+  cbind(times, id = dat_raw$design_idx) |> 
+  reshape2::melt(id.vars = "id") |> 
+  group_by(id) |> 
+  distinct(value) |> 
+  tidyr::drop_na() |> 
+  arrange(id, value)
 
 data <-
   list(
-    ntALL = max(dat_raw[, 1:max_t_idx], na.rm = TRUE),  # maximum time across all studies
-    t = dat_raw[, 1:max_t_idx],
-    r = dat_raw[, (max_t_idx + 1):(max_t_idx * 2)],
-    n = dat_raw[, (max_t_idx * 2 + 1):(max_t_idx * 3)],
-    na = dat_raw$na..)
+    ntALL = max(times, na.rm = TRUE),    # maximum time across all studies
+    design_idx = dat_raw$design_idx,
+    offset = c(0, which(diff(dat_raw$design_idx) == 1), nrow(dat_raw)) + 1,
+    # cumulative number of unique time points per study designs
+    nt_cumul = c(1, 1 + cumsum(pull(summarise(tlong, n())))),
+    ts = pull(tlong),            # unique times per study design
+    na = na,
+    t = times,
+    r = dat_raw[, grepl("r\\d", names(dat_raw))],
+    n = dat_raw[, grepl("n\\d", names(dat_raw))])
 
-para <- c("d", "sdALL", "sdRCT", "sdCC", "sdRC", "sdPC", "lor", "rk")
-bugs_filename <- "BUGS/VPB_main_docinfect.bugs"
+para <- c("d",      # outcome at each time, log odds of intervention k
+          "sdALL",  # overall between-trial heterogeneity
+          "sdRCT",  # RCT heterogeneity
+          "sdCC",   # heterogeneity between case-control studies
+          "sdRC",   # heterogeneity between retrospective cohort studies
+          "sdPC",   # heterogeneity between prospective cohort studies
+          "lor",    # log odds ratio
+          "rk")     # rank
+
+bugs_filename <- "BUGS/bugs_code.txt"
+n.iter <- 100000
 
 jagsfit <- jags(
   data = data,
@@ -31,14 +60,13 @@ jagsfit <- jags(
   parameters.to.save = para,
   model.file = bugs_filename,
   n.chains = 3,
-  n.iter = 100000,
-  n.burnin = floor(100000 / 5),
-  n.thin = max(1, floor((
-    100000 - floor(100000 / 5)) / 1000)),
+  n.iter = n.iter,
+  n.burnin = floor(n.iter / 5),
+  n.thin = max(1, floor((n.iter - floor(n.iter / 5)) / 1000)),
   DIC = TRUE,
   working.directory = NULL,
   jags.seed = 123,
-  refresh = 100000 / 50,
+  refresh = n.iter / 50,
   progress.bar = "text",
   digits = 5,
   RNGname = c(
@@ -48,6 +76,4 @@ jagsfit <- jags(
     "Mersenne-Twister"),
   jags.module = c("glm", "dic"),
   quiet = FALSE)
-
-traceplot(jagsfit)
 
