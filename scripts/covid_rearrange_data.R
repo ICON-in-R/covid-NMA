d25b4fe5a2719d775ca8fbf2aa14fd033399add3
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-06-20 17:53:35 +0100

    delete stuff that should have been cleaning in xl already

--- scripts/covid_rearrange_data.R
+++ scripts/covid_rearrange_data.R
@@ -6,11 +6,10 @@ library(dplyr)
 
 
 filename <-
-  "../../ICON/data/4428_0013_Covid_Vaccine_Spikevax_DAT_All included studies_v0.4_06Junel2023_NMA_MN.xlsx"
+  "../../ICON/data/4428_0013_Covid_Vaccine_Spikevax_DAT_All included studies_v0.4_09Junel2023_NMA_MN_AC_SK.xlsx"
 
 xl_data <- readxl::read_xlsx(path = filename,
-                             # sheet = "for Nathan",
-                             sheet = 6,
+                             sheet = "for Nathan",
                              range = "A3:BW743")
 
 # clean column names
@@ -18,7 +17,7 @@ xl_data <- readxl::read_xlsx(path = filename,
 colnamesA3 <- names(xl_data)
 
 colnamesA2 <- readxl::read_xlsx(path = filename,
-                                sheet = 6,
+                                sheet = "for Nathan",
                                 range = "A2:BW743") |> colnames()
 
 colnamesA3[grepl(pattern = "\\.\\.\\.", colnamesA3)] <- NA
@@ -28,15 +27,16 @@ colnames(xl_data) <- coalesce(colnamesA3, colnamesA2)
 
 # filter rows
 
+# run basecase scenario?
+basecase <- FALSE
+
 dat <-
   xl_data |> 
   data.frame(check.names = TRUE) |>
   dplyr::filter(Included.in.NMA == 1,
-                is.na(Subgroup.Outcome.not.chosen.for.NMA) |
-                  Subgroup.Outcome.not.chosen.for.NMA == " " |
-                  Subgroup.Outcome.not.chosen.for.NMA == "Base case",
-                VE.against.infection == "Y") |>
-  select(Ref.ID, Study.design, Intervention.name..standardized., Total.N, n.of.events)
+                if (basecase) Base.case == "X" else TRUE,
+                VE.against.infection == "Y") |> 
+  select(Ref.ID, Study.design, Design.ID, Intervention.name..standardized., Total.N, n.of.events)
 
 
 # dat <- read.csv(file = "data/cleaned_covid_data.csv")
@@ -46,16 +46,6 @@ dat_clean <-
   mutate(
     n.of.events = ifelse(n.of.events == "NR", NA, n.of.events),
     Total.N = ifelse(Total.N == "NR", NA, Total.N),
-    ##TODO: remove *
-    Intervention.name..standardized. =
-      ifelse(Intervention.name..standardized. == "Moderna (mRNA-1273)",
-             "Moderna",
-             ifelse(Intervention.name..standardized. == "No vaccine",
-                    yes = "Unvaccinated",
-                    no = Intervention.name..standardized.)),
-    Intervention.name..standardized. = ifelse(Intervention.name..standardized. == "Pfizer-BioNTech\r\nCoronaVac",
-                                              yes = "PfizerBiontech",
-                                              no = Intervention.name..standardized.),
     Intervention.name..standardized. = factor(Intervention.name..standardized.),
     interv_id = as.numeric(Intervention.name..standardized.),
     Study.design = ifelse(Study.design %in% c("Prospective cohort study",
