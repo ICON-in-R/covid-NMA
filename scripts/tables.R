8e61aa74292faed37ad6136b943abc6920d244e5
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-07-13 09:49:29 +0100

    drafting sucra table with uncertainty

--- scripts/tables.R
+++ scripts/tables.R
@@ -90,10 +90,22 @@ for (outcome in outcome_names) {
   
   tab_sucra <- 
     data.frame(Intervention = tx_levels,
-               SUCRA = c(
-                 round(mean(simsmatrix[, "SUCRA[1]"] == 1), 2),
-                 round(mean(simsmatrix[, "SUCRA[2]"] == 1), 2),
-                 round(mean(simsmatrix[, "SUCRA[3]"] == 1), 2))) |> 
+               mean = c(
+                 round(mean(simsmatrix[, "SUCRA[1]"]), 2),
+                 round(mean(simsmatrix[, "SUCRA[2]"]), 2),
+                 round(mean(simsmatrix[, "SUCRA[3]"]), 2)),
+               low = c(
+                 round(quantile(simsmatrix[, "SUCRA[1]"], probs = 0.025), 2),
+                 round(quantile(simsmatrix[, "SUCRA[2]"], probs = 0.025), 2),
+                 round(quantile(simsmatrix[, "SUCRA[3]"], probs = 0.025), 2)),
+               upp = c(
+                 round(quantile(simsmatrix[, "SUCRA[1]"], probs = 0.975), 2),
+                 round(quantile(simsmatrix[, "SUCRA[2]"], probs = 0.975), 2),
+                 round(quantile(simsmatrix[, "SUCRA[3]"], probs = 0.975), 2))) |>
+    # concatenate values with bounds in brackets
+    mutate('SUCRA'
+           = paste0(`mean`, " (", `low`, ",", `upp`, ")")) |> 
+    select(-mean, -low, -upp) |> 
     # rename vaccines
     mutate(Intervention = stringr::str_replace_all(Intervention, full_tx_levels)) 
   
