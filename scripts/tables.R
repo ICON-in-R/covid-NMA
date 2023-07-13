6ed235295335bacbcedf956ff0c032101110a225
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-07-13 09:06:15 +0100

    created new tables including: * SUCRE table * vaccine efficacy table

--- scripts/tables.R
+++ scripts/tables.R
@@ -1,16 +1,20 @@
 
-# tables using BUGS output
+# summary tables using BUGS output
 
 library(tidyr)
+library(reshape2)
+library(dplyr)
 
 outcome_names <-
   c("COVID_infection",
     "Symptomatic_infection",
     "Severe_Infection_All",
+    "Severe_infections__WHO_ICU_addmission_",
     "Hospitalizations",
     "Deaths")
 
 tx_levels <- c("placebo", "pfizer", "moderna")
+full_tx_levels <- c(placebo = "Placebo/Unvaccinated", pfizer = "Comirnaty", moderna = "Spikevax")
 
 for (outcome in outcome_names) {
   load(glue::glue("data/jagsfit_{outcome}.RData"))
@@ -18,7 +22,7 @@ for (outcome in outcome_names) {
   BUGSoutput <- jagsfit$BUGSoutput
   simsmatrix <- BUGSoutput$sims.matrix
   
-  # CROSS-TABLE FOR ODDS RATIO
+  ## CROSS-TABLE FOR ODDS RATIO
   
   tab_dat <-
     simsmatrix |> 
@@ -46,12 +50,14 @@ for (outcome in outcome_names) {
     mutate(ref = factor(ref, levels = tx_levels),
            comp = factor(comp, levels = tx_levels)) |> 
     select(-`2.5`, -`50`, -`97.5`) |> 
-    dcast(comp ~ ref)
-  
+    dcast(comp ~ ref) |> 
+    # rename vaccines
+    mutate(comp = stringr::str_replace_all(tab_dat$comp, full_tx_levels)) |> 
+    rename_with(.cols = names(full_tx_levels), ~full_tx_levels)
+    
   write.csv(tab_dat, file = glue::glue("tables/TableOR_{outcome}.csv"))
   
-  
-  # CROSS-TABLE FOR PROBABILITY BETTER
+  ## CROSS-TABLE FOR PROBABILITY BETTER
   
   tab_better <-
     simsmatrix |> 
@@ -73,9 +79,49 @@ for (outcome in outcome_names) {
     separate_wider_delim(cols = treatment, delim = "_", names = c("ref", "comp")) |> 
     mutate(ref = factor(ref, levels = tx_levels),
            comp = factor(comp, levels = tx_levels)) |> 
-    dcast(comp ~ ref)
+    dcast(comp ~ ref) |> 
+    # rename vaccines
+    mutate(comp = stringr::str_replace_all(tab_dat$comp, full_tx_levels)) |> 
+    rename_with(.cols = names(full_tx_levels), ~full_tx_levels)
   
   write.csv(tab_better, file = glue::glue("tables/TableBetter_{outcome}.csv"))
+  
+  ## SUCRE TABLE
+  
+  tab_sucre <- 
+    data.frame(Intervention = tx_levels,
+               SUCRE = c(
+                 round(mean(simsmatrix[, "SUCRA[1]"] == 1), 2),
+                 round(mean(simsmatrix[, "SUCRA[2]"] == 1), 2),
+                 round(mean(simsmatrix[, "SUCRA[3]"] == 1), 2))) |> 
+    # rename vaccines
+    mutate(Intervention = stringr::str_replace_all(tab_dat$comp, full_tx_levels)) 
+  
+  write.csv(tab_sucre, file = glue::glue("tables/table_sucre_{outcome}.csv"))
+  
+  ## VACCINE EFFICACY TABLE
+  
+  tab_vacc_effic <- 
+    simsmatrix |> 
+    as.data.frame() |> 
+    mutate(or12 = 1 - exp(`lor[1,2]`),
+           or13 = 1 - exp(`lor[1,3]`)) |> 
+    # summarise to median, lower and upper
+    reframe(pfizer = round(quantile(or12, c(0.25, 0.5, 0.75)), 2),
+            moderna = round(quantile(or13, c(0.25, 0.5, 0.75)), 2),
+            q = c(2.5, 50, 97.5)) |> 
+    mutate(placebo = c(1,1,1)) |> 
+    select(placebo, pfizer, moderna, q) |> 
+    reshape2::melt(id.vars = "q", variable.name = "Intervention") |> 
+    dcast(Intervention ~ q) |> 
+    # concatenate values with bounds in brackets
+    mutate('Placebo/Unvaccinated'
+           = paste0(`50`, " (", `2.5`, ",", `97.5`, ")")) |> 
+    select(-`2.5`, -`50`, -`97.5`) |> 
+    # rename vaccines
+    mutate(Intervention = stringr::str_replace_all(tab_dat$comp, full_tx_levels)) 
+  
+  write.csv(tab_vacc_effic, file = glue::glue("tables/table_vacc_effic_{outcome}.csv"))
 }
 
 
